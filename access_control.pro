adf7f20fb27a2d1724fe85d7a95e416ea00d8e77
scheho <scheho@gmail.com>
2012-06-03 10:34:13 +0900

    Revert "망할.. 디비어케넣냐"
    This reverts commit 5bb507f145fe69d62b919753403f4ed7287ffebf.

--- access_control.pro
+++ access_control.pro
@@ -3,14 +3,10 @@ LANGUAGE	= C++
 
 CONFIG	+= qt warn_on release
 
-HEADERS	+= data/common.h
-
-SOURCES	+= main.cpp \
-	data/common.cpp
+SOURCES	+= main.cpp
 
 FORMS	= mainwnd.ui
 
-DBFILE	= access_control.db
 unix {
   UI_DIR = .ui
   MOC_DIR = .moc
--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@ int main( int argc, char ** argv )
     QApplication a( argc, argv );
     MainWnd w;
     w.show();
-    if (  ! createConnections("database.sqlite"))
-	return 1;
     a.connect( &a, SIGNAL( lastWindowClosed() ), &a, SLOT( quit() ) );
     return a.exec();
 }
